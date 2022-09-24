85ec9f0935d6d55d2f60f097a50955a1a81d6ff5
Gerardo Miranda <gerardodev25@gmail.com>
2022-09-23 21:06:24 -0400

    React.useState

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/ProductItem.scss';
 
-import bt_add_to_cart_icon from '@icons/bt_add_to_cart.svg'
+import bt_add_to_cart_icon from '@icons/bt_add_to_cart.svg';
 
 export const ProductItem = () => {
-	return (
-		<div className="ProductItem">
-			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
-			<div className="product-info">
-				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
-				</div>
-				<figure>
-					<img src={bt_add_to_cart_icon} alt="" />
-				</figure>
-			</div>
-		</div>
-	);
-}
+  const [card, SetCard] = useState('');
+
+  const handleCard = () => {
+    SetCard('hola mundo');
+  };
+
+  return (
+    <div className="ProductItem">
+      <img
+        src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt=""
+      />
+      <div className="product-info">
+        <div>
+          <p>$120,00</p>
+          <p>Bike</p>
+        </div>
+        <figure onClick={handleCard}>
+          <img src={bt_add_to_cart_icon} alt="" />
+        </figure>
+				{card}
+      </div>
+    </div>
+  );
+};
