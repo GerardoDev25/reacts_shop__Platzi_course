e9109df20ac5a90c56c8fa6d3b0ba0cc157083d7
Gerardo Miranda <gerardodev25@gmail.com>
2022-09-26 23:02:07 -0400

    Calculando el precio total

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -8,6 +8,11 @@ import { AppContext } from '../context/AppContext';
 export const MyOrder = () => {
   const { state } = useContext(AppContext);
 
+  const sumTotal = () => {
+    const reducer = (accumalator, currentValue) => accumalator + currentValue.price;
+    const sum = state.cart.reduce(reducer, 0);
+    return sum;
+  };
   return (
     <aside className="MyOrder">
       <div className="title-container">
@@ -22,7 +27,7 @@ export const MyOrder = () => {
           <p>
             <span>Total</span>
           </p>
-          <p>$560.00</p>
+          <p>${sumTotal()}</p>
         </div>
         <button className="primary-button">Checkout</button>
       </div>
